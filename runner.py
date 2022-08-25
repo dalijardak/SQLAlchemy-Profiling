66c0eab2bcbae6b9996caf57c583795a71c97de8
Dali <mohamedali.jardak@insat.u-carthage.tn>
2022-08-25 18:47:53 +0200

    :rocket: Launch file

--- runner.py
+++ runner.py
@@ -1,12 +1,11 @@
-from __init__ import create_app, db
+from app import create_app, db
 
-app_runner = create_app('config.DevConfiguration')
+app_runner = create_app("config.DevConfiguration")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ctx = app_runner.app_context()
     ctx.push()
     # Create tables
     db.create_all()
     app_runner.run(debug=True)
     ctx.pop()
-
